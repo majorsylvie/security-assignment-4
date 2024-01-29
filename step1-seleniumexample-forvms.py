8befc05e52f928dedbf7bd38246cc732084605f3
sylv <sylv@secvm.ur>
2024-01-29 01:10:30 +0000

    more zach code in the correct file

--- step1-seleniumexample-forvms.py
+++ step1-seleniumexample-forvms.py
@@ -1,4 +1,8 @@
+# TAKEN FROM ZACHARY ROTHSTEIN'S VERY KIND ED POST
+# https://edstem.org/us/courses/51032/discussion/4207840?comment=9704860
+# https://people.cs.uchicago.edu/~zrothstein/newSeleniumVMCode.txt
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 from pyvirtualdisplay import Display
@@ -6,23 +10,31 @@ from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-driver = webdriver.Chrome()
+# Set options for headless Chrome
+options = Options()
+options.add_argument("--headless")  # Run in headless mode
+options.add_argument("--no-sandbox")  # Bypass OS security model, necessary for Docker containers
+options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
+# Assuming chromedriver is in PATH, otherwise specify the executable path
+driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.google.com")
 
-# print out the page title, stored in the driver object:
+# print out the page title
 title = driver.title
 print("Visited the page entitled: " + title)
 
-# Wait a second to load the entire webpage... but actually let's do 5 seconds so you see it!
+# Wait to load the page
 time.sleep(5)
 
 print("\n\nFirst, Blase wants to print out the page's HTML source:")
 rawhtml = driver.page_source
 print(rawhtml)
- 
+
 print("\n\nNow Blase wants to print out the rendered contents of the page:")
 contents = driver.find_element(By.XPATH, "/html/body").text
 print(contents)
 
 driver.quit()
+display.stop()
