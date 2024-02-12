0d715c8a5a997787144e9005fd090b6a3e03ba40
fkbad <fkb@uchicago.edu>
2024-02-11 19:46:32 -0600

    678 nodes 1736 edges

--- 26-assignment5.2-ghost2-boogaloo/step1makegraph.py
+++ 26-assignment5.2-ghost2-boogaloo/step1makegraph.py
@@ -1,4 +1,8 @@
-from typing import List
+import json
+from os import getloadavg
+import networkx as nx
+from typing import Dict, List, Optional
+from typing_extensions import Set
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import requests
@@ -119,7 +123,125 @@ def get_neighbors_from_hash_value(hash=None):
 
     return output_list
 
+def dfs(start=None,depth_limit=22):
+    mapping = {}
+    dfs_recursive_helper(mapping=mapping, curr_depth=0,depth_limit=depth_limit)
+    return mapping
+
+def dfs_recursive_helper(mapping: Dict[int,int] = None,
+                         curr_page: Optional[int]=None,
+                         visited: Optional[Set[int]] = None,
+                         curr_depth: int=0,
+                         depth_limit: int =22):
+
+
+    """
+    function to recurse through blase.courses webpages until the depth limit is reached
+    recording the mapping of hash value to available hyperlinks in a dictionary
+
+    depth is calculated as "total number of clicks"
+    where the ghost is assumed to start on the index page, having done 0 clicks
+        thus, clicking on the one link on index to the 5235 webpage would be click 1
+
+    Inputs:
+        curr_page: hash value of current node, if None is provided, starts at the index
+        visited: a visited set of hash values, if None is provided, will use an empty set
+        mapping: dictionary mapping hashvalues to hashvalues, based on what pages link where
+        curr_depth: the current depth, as defined above
+        depth_limit: fixed limit on the depth  recursion will stop either when there are no more nodes, or the depth limit is reached
+
+    Outputs:
+        nothing, mapping variable is updated in place
+    """
+    # visited set of hash values
+    if visited is None:
+        visited = set()
+
+    if mapping is None:
+        mapping = {}
+
+    neighbors = get_neighbors_from_hash_value(hash=curr_page)
+
+    # now that we've recorded the neighbors, we've successfully visited this link
+    # thus add it to the set
+    # print(f"curr_page : {curr_page} with type: {type(curr_page)}")
+    visited.add(curr_page)
+    # print(visited)
+
+
+    # if this is the last (aka 22nd) click
+    if curr_depth == depth_limit:
+        mapping[curr_page] = []
+        return
+
+    # similarly, now that we've visited this link, add it's neighbors to the growing mapping
+    mapping[curr_page] = neighbors
+
+    # otherwise, if we still have more clicks to do, recurse down the neighbors
+    # incrementing the click count by one to conceptually represent going from
+    # the curr node (which was click number curr_depth)
+    # and clicking on another node (setting current node to the neighbor hash value,
+    # and increasing click count by one)
+    for hash_value in neighbors:
+        if hash_value in visited:
+            continue
+        new_depth = curr_depth + 1
+        # recurse!! woooo
+        dfs_recursive_helper( curr_page=hash_value, visited=visited, mapping=mapping, curr_depth=new_depth, depth_limit=depth_limit )
+
+
+
+
+
+
+
+
+def test_neighbors():
+    n= get_neighbors_from_hash_value(hash=9120766452599220771)
+    for a in n:
+        ns = get_neighbors_from_hash_value(hash=a)
+        print(f'LAYER DEEPER: {a}')
+        print(ns)
+
+    print(n)
+
+
+def make_digraph_from_json(json_filepath='22_depth_traversal.json'):
+    """
+    function to take in a json file from the output of dfs()
+    and use NetworkX to make a directed graph modeling hyperlinks
+    betweeen blase.courses classes.
+
+    This is to then be used to construct a visual representation using graphviz
+    """
+    # first get the json into memory
+    with open(json_filepath, 'r') as readfile:
+        hyperlink_json = json.load(readfile)
+
+    print(len(hyperlink_json.keys()))
+
+    G = nx.DiGraph()
+    for source,hyperlinks in hyperlink_json.items():
+        # print(f"{source} : {hyperlinks}")
+        for hash_value in hyperlinks:
+            edge = (source,hash_value)
+            G.add_edge(*edge)
+
+    print(f"node count: {len(G.nodes)}")
+    return G
+
+
+def traverse_and_save_json(depth_limit=22):
+    mapping = dfs(depth_limit=depth_limit)
+    # print(mapping)
+    filepath = f'{depth_limit}_depth_traversal.json'
+    with open(filepath, 'w') as writefile:
+        json.dump(mapping,fp=writefile,indent=2)
+
+    return filepath
+
 
 if __name__ == "__main__":
-    n= get_neighbors_from_hash_value(hash=9120766452599220771)
-    print(n)
+    json_filepath = traverse_and_save_json(depth_limit=22)
+    make_digraph_from_json(json_filepath)
+    # make_digraph_from_json('22_depth_traversal.json')
